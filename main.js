7faa20c3c056e3919d1eb36dc25b9bedd47b439d
Denis Alves Coimbra <d.alves.coimbra@bol.com.br>
2023-07-21 23:42:28 -0300

    Finalizado o projeto

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
-function tocaSom (idElementoAudio){
-    document.querySelector(idElementoAudio).play();
+function tocaSom (seletorAudio){
+    const elemento = document.querySelector(seletorAudio);    
+
+    if (elemento && elemento.localName === 'audio') {
+        elemento.play();
+    } else {
+        alert('Elemento não encontrado ou seletor invalido');
+    }
 }
 
 const listaDeTeclas = document.querySelectorAll(".tecla");
@@ -17,6 +23,16 @@ for(let contador = 0; contador < listaDeTeclas.length; contador++){
         tocaSom(idAudio)
     };
     //console.log(contador);
+
+    tecla.onkeydown = function(evento) {
+        //console.log(evento.code)
+        if(evento.code === "Space" || evento.code === "Enter"){
+            tecla.classList.add('ativa');
+        }
+    }
+    tecla.onkeyup = function() {
+        tecla.classList.remove('ativa');
+    }
 }
 
 
